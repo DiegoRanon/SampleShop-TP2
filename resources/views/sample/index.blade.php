57100884d68cd2b1fee0bbc90562a80a9750322d
Konan <mastercelis8891@gmail.com>
2023-10-28 11:24:25 -0400

    Application Css

--- resources/views/sample/index.blade.php
+++ resources/views/sample/index.blade.php
@@ -1,53 +1,37 @@
 @extends('layouts.app')
 
-    
 @section('content')
 
-    <div class="row">
+<div class="header">
+    <h1>Mon premier blog avec Laravel</h1>
+</div>
 
-        <div class="col-lg-10">
-            <h2>Mon premier blog avec Laravel</h2>
-        </div>
-
-        <div class="col-lg-2">
-            <a class="btn btn-success" href="{{ url('sample/create') }}">Ajouter un sample</a>
-        </div>
-
+<div class="row">
+    <div class="col-lg-2">
+        <a class="button" href="{{ url('sample/create') }}">Ajouter un sample</a>
     </div>
+</div>
 
-
-
-    @if ($message = Session::get('success'))
-
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-
-    @endif
-
-
+@if ($message = Session::get('success'))
+<div class="alert-success">
+    <p>{{ $message }}</p>
+</div>
+@endif
 
 <div class="container">
     <div class="row">
         @foreach ($samples as $index => $sample)
         <div class="col-md-4">
-            <div class="card card-body">
+            <div class="sample-card">
                 <a href="{{ url('sample/'. $sample->id) }}">
-                <h2>
-                        {{ $sample->title }}
-                    </h2>
-                   
+                    <h2>{{ $sample->title }}</h2>
                 </a>
                 {{ $sample->content }}
-               
-
-          
-            <a href="{{ url('sample/'. $sample->id) }}" class="btn btn-outline-primary">En savoir plus</a>
+                <a href="{{ url('sample/'. $sample->id) }}" class="en-savoir-plus">En savoir plus</a>
             </div>
         </div>
         @endforeach
     </div>
 </div>
 
-
-@endsection 
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,11 @@ Auth::routes();
 
 Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
